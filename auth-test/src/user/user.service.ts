7a755b1403d7b30e0822fedba646936334b7de70
yunjae62 <wodbs7758@naver.com>
2024-11-29 19:19:51 +0900

    feat: 유저 서비스 추가

--- auth-test/src/user/user.service.ts
+++ auth-test/src/user/user.service.ts
@@ -7,4 +7,23 @@ export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
+
+  async createUser(user: User): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async getUser(email: string): Promise<User> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async updateUser(email: string, _user: User) {
+    const user: User = await this.getUser(email);
+    user.username = _user.username;
+    user.password = _user.password;
+    this.userRepository.save(user);
+  }
+
+  deleteUser(email: string) {
+    return this.userRepository.delete({ email });
+  }
 }
