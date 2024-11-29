51023d06670d01a104dd10cecce9f8a866d79a93
yunjae62 <wodbs7758@naver.com>
2024-11-29 19:10:46 +0900

    feat: User 엔티티 추가

--- auth-test/src/app.module.ts
+++ auth-test/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'auth-test.sqlite',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
