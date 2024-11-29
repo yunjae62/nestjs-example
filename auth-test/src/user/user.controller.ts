8df09d86f7083ca937bac8a14aa5bab01c1b3def
yunjae62 <wodbs7758@naver.com>
2024-11-30 01:43:55 +0900

    feat: user controller 추가

--- auth-test/src/user/user.controller.ts
+++ auth-test/src/user/user.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {
+  }
+
+  @Post('/create')
+  createUser(@Body() user: User) {
+    return this.userService.createUser(user);
+  }
+
+  @Get('/getUser/:email')
+  async getUser(@Param('email') email: string) {
+    return await this.userService.getUser(email);
+  }
+
+  @Put('/update/:email')
+  updateUser(@Param('email') email: string, @Body() user: User) {
+    return this.userService.updateUser(email, user);
+  }
+
+  @Delete('/delete/:email')
+  deleteUser(@Param('email') email: string) {
+    return this.userService.deleteUser(email);
+  }
+}
