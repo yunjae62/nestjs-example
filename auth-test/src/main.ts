2a389709d15b94ba188ac94f0e8c09f9c8712401
yunjae62 <wodbs7758@naver.com>
2024-11-30 13:55:57 +0900

    config: passport 라이브러리 초기화

--- auth-test/src/main.ts
+++ auth-test/src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.use(
+    session({
+      secret: 'very-important-secret', // 내부적으로 암호화에 쓰이는 키
+      resave: false, // 세션 항상 저장할지 여부
+      saveUninitialized: false, // 세션 저장 전 빈 값 저장할지 여부
+      cookie: { maxAge: 3_600_000 }, // 유효시간 1시간
+    }),
+  );
+  app.use(passport.initialize()); // 패스포트 초기화
+  app.use(passport.session()); // 세션 저장소 초기화
   await app.listen(process.env.PORT ?? 3000);
 }
 
