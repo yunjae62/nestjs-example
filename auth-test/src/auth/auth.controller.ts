5d7bcd9f9a5402107b83b11c641331e595523e77
yunjae62 <wodbs7758@naver.com>
2024-11-30 13:48:12 +0900

    feat: 인증 가드 추가

--- auth-test/src/auth/auth.controller.ts
+++ auth-test/src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/user.dto';
 import { Request, Response } from 'express';
+import { LoginGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -27,5 +36,29 @@ export class AuthController {
     }
 
     return res.send({ message: 'login success' });
+  }
+
+  @UseGuards(LoginGuard)
+  @Post('login2')
+  async login2(@Req() req: Request, @Res() res: Response) {
+    const userInfo = await this.authService.validateUser(
+      req.body.email,
+      req.body.password,
+    );
+
+    if (userInfo) {
+      res.cookie('login', JSON.stringify(userInfo), {
+        httpOnly: false,
+        maxAge: 1000 * 10,
+      });
+    }
+
+    return res.send({ message: 'login success' });
+  }
+
+  @UseGuards(LoginGuard)
+  @Get('test-guard')
+  testGuard() {
+    return '로그인 되었을 때만 이 글이 보입니다.';
   }
 }
